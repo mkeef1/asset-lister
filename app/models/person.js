f8b506556fce9d37830212504959e53f1bfc7e87
Matt Keef <mkeef1@gmail.com>
2014-08-17 00:49:06 -0500

    Most of model and test finished

--- app/models/person.js
+++ app/models/person.js
@@ -1,6 +1,13 @@
 'use strict';
 
-function Person(){
+var Mongo = require('mongodb'),
+    _     = require('lodash');
+
+function Person(p){
+  this.name = p.name;
+  this.cash = p.cash;
+  this.photo = p.photo;
+  this.items = [];
 }
 
 Object.defineProperty(Person, 'collection', {
@@ -11,5 +18,27 @@ Person.all = function(cb){
   Person.collection.find().toArray(cb);
 };
 
+Person.create = function(o, cb){
+  var a = new Person(o);
+  Person.collection.save(a, cb);
+};
+
+Person.findById = function(id, cb){
+  var _id = Mongo.ObjectID;
+  Person.collection.findOne({_id:_id}, function(err, obj){
+    var person = changePrototype(obj);
+    cb(person);
+  });
+};
+
+Person.prototype.addItem = function(item, cb){
+  this.items.push(item);
+  Person.collection.update({_id:this._id}, {$push:{items:item}}, cb);
+};
 module.exports = Person;
 
+//Private function//
+
+function changePrototype(obj){
+  return _.create(Person.prototype, obj);
+}
--- test/unit/person.js
+++ test/unit/person.js
@@ -7,7 +7,8 @@ var expect    = require('chai').expect,
     Person    = require('../../app/models/person'),
     dbConnect = require('../../app/lib/mongodb'),
     cp        = require('child_process'),
-    db        = 'template-test';
+    db        = 'people-test',
+    Mongo     = require('mongodb');
 
 describe('Person', function(){
   before(function(done){
@@ -24,7 +25,7 @@ describe('Person', function(){
 
   describe('constructor', function(){
     it('should create a new Person object', function(){
-      var p = new Person();
+      var p = new Person({name:'Bob Jones', cash:1000, photo:'bob.jpg', items:['a1', 'a2', 'a3']});
       expect(p).to.be.instanceof(Person);
     });
   });
@@ -32,10 +33,41 @@ describe('Person', function(){
   describe('.all', function(){
     it('should get all people', function(done){
       Person.all(function(err, people){
-        expect(people).to.have.length(2);
+        expect(people).to.have.length(3);
         done();
       });
     });
   });
+
+  describe('.create', function(){
+    it('should create a new person object', function(done){
+      var p = new Person({name:'Bob Jones', cash:1000, photo:'bob.jpg', items:['a1', 'a2', 'a3']});
+      Person.create(p, function(err, person){
+        expect(p).to.be.instanceof(Person);
+        done();
+      });
+    });
+  });
+
+  describe('.findById', function(){
+    it('should find a person by their id', function(done){
+      Person.findById(Mongo.ObjectID('000000000000000000000001'), function(person){
+        expect(person.name).to.equal('Bob Jones');
+        expect(person).to.be.instanceof(Person);
+        done();
+      });
+    });
+  });
+
+  /*describe('#addItem', function(){
+    it('should add an item to a person', function(done){
+      var i = {name:'couch', photo:'couch.jpg', count:1, value:500, total:500},
+      Person.addItem(i, function(err, person){
+        expect(person.items).to.have.length(4);
+        console.log(items);
+        done();
+      });
+    });
+  });*/
 });
 
